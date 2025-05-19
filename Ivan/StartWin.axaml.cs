e5ce39bf4e2ce91b72cabdc4418665242d8b390a
Михатл <mikhailcher2006@gmail.com>
2025-05-19 19:57:04 +0300

    Создана верстка окна входа и подключен MySQL

--- Ivan/StartWin.axaml.cs
+++ Ivan/StartWin.axaml.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MySql.Data;
 
 namespace Ivan;
 
@@ -9,5 +11,13 @@ public partial class StartWin : Window
     public StartWin()
     {
         InitializeComponent();
+    }
+
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+        
+        
     }
 }
