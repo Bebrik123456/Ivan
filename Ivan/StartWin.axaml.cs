6f3543119be4511bbad8a60cc54564181830d75f
Михатл <mikhailcher2006@gmail.com>
2025-05-19 14:14:47 +0300

    Нулевой проект

---
+++ Ivan/StartWin.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ivan;
+
+public partial class StartWin : Window
+{
+    public StartWin()
+    {
+        InitializeComponent();
+    }
+}
